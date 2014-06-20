bc8e25bbdda37f7f41edb2a566d1dddf8d14ace6
Oren Leiman <oren@neutral.io>
2014-06-20 16:16:53 -0700

    BLK-7 YouTube block
    Signed-off-by: Oren Leiman <oren@neutral.io>

---
+++ tests/test_youtube_block.py
@@ -0,0 +1,54 @@
+from unittest.mock import patch
+from requests import Response
+from datetime import datetime, timedelta
+from youtube.youtube_block import YouTube
+from nio.util.support.block_test_case import NIOBlockTestCase
+from nio.modules.threading.imports import Event
+
+
+class YTTestBlk(YouTube):
+    def __init__(self, event):
+        super().__init__()
+        self._event = event
+
+    def _paging(self):
+        self._event.set()
+
+class TestYouTube(NIOBlockTestCase):
+
+    @patch("requests.get")
+    @patch("requests.Response.json")
+    @patch("youtube.youtube_block.YouTube.created_epoch")
+    def test_process_responses(self, mock_epoch, mock_json, mock_get):
+        mock_get.return_value = Response()
+        mock_get.return_value.status_code = 200
+        mock_epoch.return_value = 23
+        mock_json.return_value = {
+            'items': [
+                {'snippet': {'key': 'val'}}
+            ]
+        }
+        e = Event()
+        blk = YTTestBlk(e)
+        self.configure_block(blk, {
+            "log_level": "DEBUG",
+            "polling_interval": {
+                "seconds": 1
+            },
+            "retry_interval": {
+                "seconds": 1
+            },
+            "queries": [
+                "foobar"
+            ],
+            "limit": 2,
+        })
+        blk._freshest = [22]
+
+        blk.start()
+        e.wait(2)
+
+        self.assertEqual(blk._freshest, [23])
+        self.assert_num_signals_notified(1)
+
+        blk.stop()
---
+++ youtube_block.py
@@ -0,0 +1,89 @@
+from datetime import datetime
+from nio.common.discovery import Discoverable, DiscoverableType
+from .http_blocks.rest.rest_block import RESTPolling
+from nio.metadata.properties.string import StringProperty
+from nio.metadata.properties.timedelta import TimeDeltaProperty
+from nio.metadata.properties.int import IntProperty
+from nio.metadata.properties.list import ListProperty
+from nio.common.signal.base import Signal
+
+
+class YouTubeSignal(Signal):
+    def __init(self, data):
+        for k in data:
+            setattr(self, k, data[k])
+
+
+@Discoverable(DiscoverableType.block)
+class YouTube(RESTPolling):
+    
+    URL_FORMAT = ("https://www.googleapis.com/youtube/v3/"
+                  "search?order=date&part=snippet&q={0}&maxResults={1}&key={2}")
+
+    dev_key = StringProperty(default='')
+    lookback = TimeDeltaProperty()
+    limit = IntProperty(default=20)
+    exclude = ListProperty(str)
+
+    def __init__(self):
+        super().__init__()
+        self._paging_field = 'nextPageToken'
+        self._created_field = 'publishedAt'
+        self._page_token = None
+
+    def configure(self, context):
+        super().configure(context)
+        self._smash_queries()
+        lb = self._unix_time(datetime.utcnow() - self.lookback)
+        self._freshest = [lb] * self._n_queries
+
+    def _smash_queries(self):
+        """ Helper method to take advantage of the YouTube API's multi-
+        query support, avoiding unnecessary HTTP requests.
+
+        """
+        query_str = '|'.join(self.queries)
+        if len(self.exclude) > 0:
+            excl_str = '-'.join(self.exclude)
+            query_str = '-'.join([query_str, excl_str])
+        self.queries = [query_str]
+        self._n_queries = 1
+
+    def _process_response(self, resp):
+        signals = []
+        paging = False
+        status = resp.status_code
+        if status == 304:
+            return [], paging
+
+        resp = resp.json()
+        fresh_posts = posts = [i['snippet'] for i in resp.get('items', [])]
+        self._page_token = resp.get(self._paging_field)
+        self._logger.debug("YouTube response contains %d posts" % len(posts))
+        
+        if len(posts) > 0:
+            self.update_freshness(posts)
+            fresh_posts = self.find_fresh_posts(posts)
+            paging = len(fresh_posts) == len(posts)
+
+        signals = [YouTubeSignal(p) for p in fresh_posts]
+        self._logger.debug("Found %d fresh posts" % len(signals))
+
+        return signals, paging
+
+    def _prepare_url(self, paging=False):
+        headers = {"Content-Type": "application/json"}
+        if self.etag is not None:
+            headers['If-None-Match'] = self.etag
+        if self.modified is not None:
+            headers['If-Modified-Since'] = self._modified
+
+        self.url = self.URL_FORMAT.format(
+            self.current_query,
+            self.limit,
+            self.dev_key
+        )
+        if paging:
+            self.url = "%s&pageToken=%s" % (self.url, self._page_token)
+            
+        return headers
