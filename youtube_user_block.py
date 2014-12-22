6a5ff16eabbde09027c886efdb3e49256e0c7ece
Oren Leiman <oleiman@neutral.io>
2014-12-22 14:01:07 -0800

    BLK-206 Caching for YouTubeUser username/id pairs
    Signed-off-by: Oren Leiman <oleiman@neutral.io>

--- youtube_user_block.py
+++ youtube_user_block.py
@@ -16,21 +16,30 @@ class YouTubeUser(YouTubeChannel):
         self.queries = [q for q in self.queries if q is not None]
         self._update_internal_data()
 
+    def stop(self):
+        super().stop()
+        self.persistence.save()
+
     def _get_user_channel_id(self, username):
-        headers = {"Content-Type": "application/json"}
-        url = self.USER_CHANNEL_URL.format(username, self.dev_key)
-        response = requests.get(url, headers=headers, auth=self._auth)
-        response = response.json()
-        if response.get('error') is not None:
-            self._logger.error(
-                "YouTube channel list request failed: {}, reasons: {}".format(
-                    response['error']['code'],
-                    [e['reason'] for e in response['error']['errors']]
+        _id = self.persistence.load(username)
+        if _id is None:
+            headers = {"Content-Type": "application/json"}
+            url = self.USER_CHANNEL_URL.format(username, self.dev_key)
+            response = requests.get(url, headers=headers, auth=self._auth)
+            response = response.json()
+            if response.get('error') is not None:
+                self._logger.error(
+                    "YouTube channel list request failed: {}, reasons: {}".format(
+                        response['error']['code'],
+                        [e['reason'] for e in response['error']['errors']]
+                    )
                 )
-            )
-            return None
-        channels = response.get('items')
-        return channels[0].get('id') if len(channels) > 0 else None
+                _id = None
+            else:
+                channels = response.get('items')
+                _id = channels[0].get('id') if len(channels) > 0 else None
+                self.persistence.store(username, _id)
+        return _id
 
     def _update_internal_data(self):
         self._n_queries = len(self.queries)
