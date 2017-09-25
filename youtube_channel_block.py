fb7df2c2f4965a771218c5c1b61f7598bb38fc33
neutralioJenkins <jenkins@n.io>
2017-09-25 14:14:46 -0600

    Bump version to v1.0.1

--- youtube_channel_block.py
+++ youtube_channel_block.py
@@ -11,7 +11,7 @@ class YouTubeChannel(YouTube):
                   "search?order=date&part=snippet&"
                   "type=video&maxResults={0}&channelId={1}&"
                   "key={2}")
-    version = VersionProperty("1.0.0")
+    version = VersionProperty("1.0.1")
 
     def configure(self, context):
         super().configure(context)
